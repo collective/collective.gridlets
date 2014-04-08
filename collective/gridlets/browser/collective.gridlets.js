5b390b99313b1ca78c0e8de22cf22140684e7d66
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-04-08 22:21:34 +0200

    Solve some js issues with bindings on creation and deletion of gridlets

--- collective/gridlets/browser/collective.gridlets.js
+++ collective/gridlets/browser/collective.gridlets.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   // Use bootstrap classes for add portlet dropdown
-  $('#portletmanager-collective-gridlets-portletManager').delegate('#gridportletselector a', 'click', function(event) {
+  $('.template-edit.portaltype-homepage').delegate('#gridportletselector a', 'click', function(event) {
       event.preventDefault();
       event.stopPropagation();
       $a = $(event.target);
@@ -18,6 +18,38 @@ $(document).ready(function() {
           success: function(data){
               var container = form.parents('#content');
               container.replaceWith($('#content', data));
+              $('.gridster ul').gridster({
+                  widget_margins: [10, 10],
+                  widget_base_dimensions: [150, 150],
+                  max_cols: 6,
+                  resize: {
+                    enabled: true,
+                    axes: ['x'],
+                    stop: function(e, ui, $widget) {
+                      data = {
+                        position: JSON.stringify(this.serialize())
+                      }
+                      $('#form-widgets-IGridlets-gridlets').val(data.position);
+                    }
+                  },
+                  draggable: {
+                    stop: function(event, ui) {
+                      data = {
+                        position: JSON.stringify(this.serialize())
+                      };
+                      $('#form-widgets-IGridlets-gridlets').val(data.position);
+                    }
+                  },
+                  serialize_params: function($w, wgd) {
+                    return {
+                      id: $($w).data().portlethash,
+                      col: wgd.col,
+                      row: wgd.row,
+                      size_x: wgd.size_x,
+                      size_y: wgd.size_y
+                    };
+                  }
+              });
               $('#kss-spinner').hide();
           },
           error: function(){
@@ -27,7 +59,7 @@ $(document).ready(function() {
       return false;
   });
 
-  $('.gridster').delegate('.portlet-action a', 'click', function(e){
+  $('.template-edit.portaltype-homepage').delegate('.portlet-action a', 'click', function(e){
       event.preventDefault();
       event.stopPropagation();
       $('#kss-spinner').show();
@@ -46,6 +78,7 @@ $(document).ready(function() {
               if (form.data().friendlyaction === 'delete'){
                 var gridster = $(".gridster ul").gridster().data('gridster');
                 gridster.remove_widget(form.parents('li'));
+                $('#form-widgets-IGridlets-gridlets').val(JSON.stringify(gridster.serialize()))
               }
               if (form.data().friendlyaction === 'toggle'){
                 button = form.children('a');

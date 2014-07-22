cfbbb72445feed797417efd26f12fb6657a44d9b
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-07-22 17:38:25 +0200

    Back to development: 1.0b2

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -2,6 +2,12 @@ Changelog
 =========
 
 File"setup.py",line24
+1.0b2 (unreleased)
+------------------
+
+- Nothing changed yet.
+
+
 1.0b1 (2014-07-22)
 ------------------
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0b1'
+version = '1.0b2.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
